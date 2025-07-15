7ed9ee76a17b6d8bb9760136fdca96f6722c8ef9
Pascal Birchler <pascalb@google.com>
2025-07-15 10:51:07 +0200

    Experimental HMR

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+});
