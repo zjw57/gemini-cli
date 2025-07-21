7eaa78832b500a77dd0f2c1d23adc86c692bdd5d
Karmel Allison <karmel@google.com>
2025-07-21 16:28:43 +0000

    Revert "Implement loop check with LLM (#4337)"
    This reverts commit 9dadf2295804867d44628eda32fc1e46d80f9db3.

--- packages/core/src/telemetry/types.ts
+++ packages/core/src/telemetry/types.ts
@@ -249,7 +249,6 @@ export class FlashFallbackEvent {
 export enum LoopType {
   CONSECUTIVE_IDENTICAL_TOOL_CALLS = 'consecutive_identical_tool_calls',
   CHANTING_IDENTICAL_SENTENCES = 'chanting_identical_sentences',
-  LLM_DETECTED_LOOP = 'llm_detected_loop',
 }
 
 export class LoopDetectedEvent {
