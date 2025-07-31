98d5729c0fb79045ed5f10dee815acf82cf7f4ff
christine betts <chrstn@uw.edu>
2025-07-31 12:07:56 -0400

    allow edits

--- packages/vscode-ide-companion/src/diff-manager.ts
+++ packages/vscode-ide-companion/src/diff-manager.ts
@@ -72,6 +72,12 @@ export class DiffManager {
         fileUri,
         rightDocUri,
         diffTitle,
+        {
+          preview: false,
+        },
+      );
+      await vscode.commands.executeCommand(
+        'workbench.action.files.setActiveEditorWriteableInSession',
       );
     } else {
       // If the file doesn't exist, we create it and show it directly
@@ -177,7 +183,10 @@ export class DiffManager {
         this.onDidChangeEmitter.fire({
           jsonrpc: '2.0',
           method: 'ide/diffClosed',
-          params: { filePath: diffInfo.originalFilePath },
+          params: { 
+            filePath: diffInfo.originalFilePath, 
+            newContent: document.getText(),
+          },
         });
         this.diffDocuments.delete(closedUriString);
         this.diffContentProvider.deleteContent(document.uri);
@@ -187,6 +196,26 @@ export class DiffManager {
           false,
         );
         vscode.window.showInformationMessage('Diff view closed.');
+      }
+    }
+  }
+
+  /**
+   * Called when the text document for a diff view is changed.
+   */
+  onDidChangeTextDocument(e: vscode.TextDocumentChangeEvent) {
+    const changedUriString = e.document.uri.toString();
+    if (this.diffDocuments.has(changedUriString)) {
+      const diffInfo = this.diffDocuments.get(changedUriString);
+      if (diffInfo) {
+        this.onDidChangeEmitter.fire({
+          jsonrpc: '2.0',
+          method: 'ide/diffModified',
+          params: {
+            filePath: diffInfo.originalFilePath,
+            newContent: e.document.getText(),
+          },
+        });
       }
     }
   }
