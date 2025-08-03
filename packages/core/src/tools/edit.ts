2315e80bb35269c7e14f5f5c9b8bc6c434bd4d38
Silvio Junior <silviojr.dcc@gmail.com>
2025-08-03 19:38:17 +0000

    logs as a single line

--- packages/core/src/tools/edit.ts
+++ packages/core/src/tools/edit.ts
@@ -543,7 +543,7 @@ class TestAllStrategy implements ReplaceStrategyImpl {
       }
     }
 
-    console.log(JSON.stringify({ test_all_results: allResults }, null, 2));
+    console.log(JSON.stringify({ test_all_results: allResults }));
 
     if (correctorResultToReturn) {
       return correctorResultToReturn;
