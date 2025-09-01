f331e5d5b62ad90fad29495a0b8813f1538cf25f
Billy Biggs <bbiggs@google.com>
2025-08-31 17:44:16 -0700

    Merge general settings from different configuration sources (#7528)

--- packages/cli/src/config/settings.ts
+++ packages/cli/src/config/settings.ts
@@ -303,6 +303,12 @@ function mergeSettings(
     ...user,
     ...safeWorkspaceWithoutFolderTrust,
     ...system,
+    general: {
+      ...(systemDefaults.general || {}),
+      ...(user.general || {}),
+      ...(safeWorkspaceWithoutFolderTrust.general || {}),
+      ...(system.general || {}),
+    },
     ui: {
       ...(systemDefaults.ui || {}),
       ...(user.ui || {}),
