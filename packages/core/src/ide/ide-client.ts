61577c9719bccdf7f16b857452e3a6aea06c2814
Shreya <shreyakeshive@google.com>
2025-09-09 17:58:32 -0400

    fix

--- packages/core/src/ide/ide-client.ts
+++ packages/core/src/ide/ide-client.ts
@@ -22,6 +22,7 @@ import { StdioClientTransport } from '@modelcontextprotocol/sdk/client/stdio.js'
 import * as os from 'node:os';
 import * as path from 'node:path';
 import { EnvHttpProxyAgent } from 'undici';
+import { IDE_PORT_FILE_DIR } from './constants.js'
 
 const logger = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -392,16 +393,6 @@ export class IdeClient {
     return { command, args };
   }
 
-  private async getIdeConnectionDir(): Promise<string> {
-    const dir = path.join(os.tmpdir(), '.gemini', 'ide');
-    try {
-      await fs.promises.access(dir);
-    } catch (e) {
-      logger.debug('IDE connection directory does not exist:', e);
-    }
-    return dir;
-  }
-
   private async getConnectionConfigFromFile(): Promise<
     (ConnectionConfig & { workspacePath?: string }) | undefined
   > {
@@ -421,17 +412,9 @@ export class IdeClient {
       // For newer extension versions, the file matches the following prefix. If multiple IDE windows are open, multiple files with the same prefix are expected to exist.
     }
 
-    let ideConnectionDir;
-    try {
-      ideConnectionDir = await this.getIdeConnectionDir();
-    } catch (e) {
-      logger.debug('Failed to get IDE connection directory:', e);
-      return undefined;
-    }
-
     let portFiles;
     try {
-      portFiles = await fs.promises.readdir(ideConnectionDir);
+      portFiles = await fs.promises.readdir(IDE_PORT_FILE_DIR);
     } catch (e) {
       logger.debug('Failed to read IDE connection directory:', e);
       return undefined;
@@ -451,7 +434,7 @@ export class IdeClient {
     try {
       fileContents = await Promise.all(
         matchingFiles.map((file) =>
-          fs.promises.readFile(path.join(ideConnectionDir, file), 'utf8'),
+          fs.promises.readFile(path.join(IDE_PORT_FILE_DIR, file), 'utf8'),
         ),
       );
     } catch (e) {
