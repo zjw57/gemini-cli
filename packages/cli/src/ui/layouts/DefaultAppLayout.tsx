ef76a801c425ca6048c40fe2d0a08936772d6a78
Billy Biggs <bbiggs@google.com>
2025-10-01 14:50:33 -0700

    Revert reducing margin on narrow screens  (#10375)

--- packages/cli/src/ui/layouts/DefaultAppLayout.tsx
+++ packages/cli/src/ui/layouts/DefaultAppLayout.tsx
@@ -13,13 +13,11 @@ import { Composer } from '../components/Composer.js';
 import { ExitWarning } from '../components/ExitWarning.js';
 import { useUIState } from '../contexts/UIStateContext.js';
 
-export const DefaultAppLayout: React.FC<{ width?: string }> = ({
-  width = '90%',
-}) => {
+export const DefaultAppLayout: React.FC = () => {
   const uiState = useUIState();
 
   return (
-    <Box flexDirection="column" width={width}>
+    <Box flexDirection="column" width="90%">
       <MainContent />
 
       <Box flexDirection="column" ref={uiState.mainControlsRef}>
