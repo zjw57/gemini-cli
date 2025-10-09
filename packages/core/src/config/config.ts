e847ab242631c85ade674db2b3497aefc06679f5
Hadi Minooei <minooei@google.com>
2025-10-08 21:43:22 -0700

    wip

--- packages/core/src/config/config.ts
+++ packages/core/src/config/config.ts
@@ -456,7 +456,7 @@ export class Config {
       params.enableMessageBusIntegration ?? false;
     this.enableSubagents = params.enableSubagents ?? false;
     this.enableShellOutputEfficiency =
-      params.enableShellOutputEfficiency ?? true;
+      params.enableShellOutputEfficiency ?? false;
     this.extensionManagement = params.extensionManagement ?? true;
     this.storage = new Storage(this.targetDir);
     this.enablePromptCompletion = params.enablePromptCompletion ?? false;
